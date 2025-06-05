1bb9d32bd0ab4fffebd36d805c8b344c0339c3a6
lubie-max <shaikhls2421@gmail.com>
2025-06-06 01:49:52 +0530

    .map(String::toUpperCase) :

--- src/streamAPI_Exercise/OperationsOnStringArray.java
+++ src/streamAPI_Exercise/OperationsOnStringArray.java
@@ -1,8 +1,28 @@
 package streamAPI_Exercise;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OperationsOnStringArray {
 
     public static void main(String[] args) {
+
+
+
+        List<String> languages = Arrays.asList("Python", "java", "cpp","sqL");
+        ArrayList<String> l = new ArrayList<>();
+        l.add("p");l.add("j"); l.add("c");
+
+//        Uppercase the element of  list
+
+        List<String> upperCase = languages.stream()
+                .map(String::toUpperCase)
+                .toList();
+        System.out.println("Uppercase :: ");
+        upperCase.forEach( System.out::println);
+
 
 
     }
